80aac46794ece7633c9bd038aaa6a36139913224
mrWD <lvigtor@gmail.com>
2019-08-03 11:16:23 +0300

    Clean comments.

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,10 @@ module.exports = {
       {
         test: /\.pug$/,
         oneOf: [
-          // это применяется к `<template lang="pug">` в компонентах Vue
           {
             resourceQuery: /^\?vue/,
             use: ['pug-plain-loader']
           },
-          // это применяется к импортам pug внутри JavaScript
           {
             use: ['pug-loader']
           }
